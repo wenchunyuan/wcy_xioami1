1706926c636b4eb11fe11a7186024a1924fe62ef
wenchunyuan <2944910797@qq.com>
2018-04-02 21:42:56 +0800

    1

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 //banner
 	{
-		const imgs=document.querySelectorAll(".imgbox li");   //const:不会被修改，常量
+		const imgs=document.querySelectorAll(".imgbox li");   //不会被修改，常量
 		let pagers=document.querySelectorAll(".pagebox li");
-		let benner=document.querySelector("#benner");   //设置banner，鼠标放上去，停止播放，鼠标移开开始播放。
+		let banner=document.querySelector("#banner");   //设置banner，鼠标放上去，停止播放，鼠标移开开始播放。
 		let banner_lbtn=document.querySelector(".banner_lbtn"); 
 		let banner_rbtn=document.querySelector(".banner_rbtn"); 
 
-		pagers.forEach(function(ele,index){    //形参,遍历数。
+		pagers.forEach(function(ele,index){    //形参，遍历数。
 			ele.onclick=function(){
 				for(let i=0;i<imgs.length;i++){
 					imgs[i].classList.remove("active");
 					pagers[i].classList.remove("active");   //移除active
 				}
-					this.classList.add("active");
-					imgs[index].classList.add("active");    //添加active
-					n=index;     //n从当前位子自加。
+				this.classList.add("active");
+				imgs[index].classList.add("active");    //添加active
+				n=index;     //n从当前位子自加。
 			}
 		})
 
-		// window.setInterval();//让代码自动执行   自动轮播
+		// BOM_window.setInterval();//让代码自动执行   自动轮播
 		let n=0;     //访问n，自加
 		let t=setInterval(move,3000)
 
@@ -60,7 +60,6 @@
 						// },3000)
 			t=setInterval(move,3000);
 		}
-		
 		let flag=true;
 		banner_rbtn.onclick=function(){
 			if(flag){
@@ -82,7 +81,7 @@
 		})
 	}
 
-//闪购
+//闪购--小米单品
 	{
 		const prve=document.querySelector(".buy-prev");
 		const next=document.querySelector(".buy-next");
@@ -177,54 +176,54 @@
 	}
 
 //哈利波特
-{
-	function wheel(parent){
-		let prev=parent.querySelector(".reping_lbtn");
-		let next=parent.querySelector(".reping_rbtn");
-		let inner=parent.querySelector(".reping_b2");    //da
-		let contents=parent.querySelectorAll(".reping_goodlist");   //tu
-		let pagers=parent.querySelectorAll(".reping_b25btv");
+	{
+		function wheel(parent){
+			let prev=parent.querySelector(".reping_lbtn");
+			let next=parent.querySelector(".reping_rbtn");
+			let inner=parent.querySelector(".reping_b2");    //da
+			let contents=parent.querySelectorAll(".reping_goodlist");   //tu
+			let pagers=parent.querySelectorAll(".reping_b25btv");
 
-		let n=0;
-		next.onclick=function(){
-			n++;
-			if(n===contents.length){
-				n=contents.length-1;
-				return;
-			}
-			inner.style.marginLeft=n*-296+"px";
-			pagers[n].classList.add("active");
-			pagers[n-1].classList.remove("active");
-		};
-		prev.onclick=function(){
-			n--;
-			if(n<0){
-				n=0;
-				return;
-			}
-			inner.style.marginLeft=n*-296+"px";
-			pagers[n].classList.add("active");
-			pagers[n+1].classList.remove("active");
-			obj=pagers[0];       
-		};
-		//点哪个点亮对应的图片
-		let obj=pagers[0];     //默认第一张有颜色
-		pagers.forEach(function(ele,index){
-			ele.onclick=function(){
-				obj.classList.remove("active");
-				ele.classList.add("active");
-				obj=ele;
-				inner.style.marginLeft=index*-296+"px";     //图片位移
-				n=index;     //统一下标，
-			}
-		})
+			let n=0;
+			next.onclick=function(){
+				n++;
+				if(n===contents.length){
+					n=contents.length-1;
+					return;
+				}
+				inner.style.marginLeft=n*-296+"px";
+				pagers[n].classList.add("active");
+				pagers[n-1].classList.remove("active");
+			};
+			prev.onclick=function(){
+				n--;
+				if(n<0){
+					n=0;
+					return;
+				}
+				inner.style.marginLeft=n*-296+"px";
+				pagers[n].classList.add("active");
+				pagers[n+1].classList.remove("active");
+				obj=pagers[0];
+			};
+			//点哪个点亮对应的图片
+			let obj=pagers[0];     //默认第一张有颜色
+			pagers.forEach(function(ele,index){
+				ele.onclick=function(){
+					obj.classList.remove("active");
+					ele.classList.add("active");
+					obj=ele;
+					inner.style.marginLeft=index*-296+"px";     //图片位移
+					n=index;     //统一下标，
+				}
+			})
+		}
+
+		let items=document.querySelectorAll(".reping_item");
+		items.forEach(function(ele){
+				wheel(ele);
+			})
 	}
-
-	let items=document.querySelectorAll(".reping_item");
-	items.forEach(function(ele){
-			wheel(ele);
-		})
-}
 
 //banner 侧导航
 	{
@@ -253,7 +252,7 @@
 		// let nav=document.querySelector(".nav");
 		let wenzis=document.querySelector(".nav_wenzi");
 		let nav_bottom=document.querySelector(".nav_bottom");
-		const goods=document.querySelectorAll(".nav_bottom_goodlist"); 
+		const goods=document.querySelectorAll(".nav_bottom_goodlist");
 		let types=document.querySelectorAll(".nav_wenzi span");
 
 		let obj=goods[0];                   //设置diaplay：none；确保开始不出现。
@@ -264,7 +263,7 @@
 		wenzis.onmouseleave=function(){
 			nav_bottom.style.height="0px";
 		}
-		
+
 		types.forEach(function(ele,index){
 			ele.onmouseenter=function(){
 				for(let i=0;i<types.length;i++){
@@ -275,7 +274,32 @@
 					obj=goods[index];
 				}
 				// this.classList.add("achive");
-				// goods[index].classList.add("achive"); 
+				// goods[index].classList.add("achive");
 			}
 		})
+	}
+
+//小米单品  时间
+	{
+		let hourEle=document.querySelector(".hours");
+		let minuteEle=document.querySelector(".minutes");
+		let secondEle=document.querySelector(".seconds");
+		let newT=new Date();
+		let newTate=newT.getTime();
+		let targetT=new Date(2018,4,1,0,0,0);
+		let targetTate=targetT.getTime();
+        setInterval(time,1000);
+        time();
+        function time() {
+            let newT=new Date();
+            let newTate=newT.getTime();
+            let duration=targetTate-newTate;
+            let hours=Math.floor(duration/(1000*60*60));
+            hourEle.innerHTML=hours;
+            let minutes=Math.floor(duration/(1000*60))%60;
+            minuteEle.innerHTML=minutes;
+            let seconds=Math.floor(duration/(1000))%60;
+            secondEle.innerHTML=seconds;
+
+        }
 	}
